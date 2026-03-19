daa228737c21241392f1b5d7f625e63fb058c0e3
gobdanov <gobdanov@inbox.ru>
2026-03-19 11:32:27 +0500

    Разработанные контексты данных. Белослудцева

---
+++ Classes/MessagesContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pr33.Classes.Common;
+using pr33.Models;
+
+namespace pr33.Classes
+{
+    public class MessagesContext : DbContext
+    {
+        /// <summary> Данные из БД
+        public DbSet<Messages> Messages { get; set; }
+
+        /// <summary> Конструктор контекста
+        public MessagesContext() =>
+            Database.EnsureCreated();
+
+        /// <summary> Конфигурация подключения
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            // Говорим что используем SQL Server со следующей конфигурацией
+            optionsBuilder.UseSqlServer(Config.config);
+    }
+}
---
+++ Classes/UsersContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pr33.Classes.Common;
+using pr33.Models;
+
+namespace pr33.Classes
+{
+    public class UsersContext : DbContext
+    {
+        /// <summary> Данные из БД
+        public DbSet<Users> Users { get; set; }
+        /// <summary> Конструктор контекста
+        public UsersContext() =>
+            // Проверяем подключены ли мы к БД, если не подключены, подключаемся
+            Database.EnsureCreated();
+
+        /// <summary> Конфигурация подключения
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            // Говорим что используем SQL Server со следующей конфигурацией
+            optionsBuilder.UseSqlServer(Config.config);
+    }
+}
