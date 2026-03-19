fef902caddfcb219ae2db827bfda12748c29fbc3
gobdanov <gobdanov@inbox.ru>
2026-03-19 11:21:19 +0500

    Разработанные модели. Белослудцева

---
+++ Models/Messages.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr33.Models
+{
+    class Messages
+    {
+        /// <summary> Код сообщения
+        public int Id { get; set; }
+        /// <summary> Отправитель
+        public int UserFrom { get; set; }
+        /// <summary> Получатель
+        public int UserTo { get; set; }
+        /// <summary> Текст сообщения
+        public string Message { get; set; }
+        /// <summary> Конструктор для сообщения
+        public Messages(int UserFrom, int UserTo, string Message)
+        {
+            this.UserFrom = UserFrom;
+            this.UserTo = UserTo;
+            this.Message = Message;
+        }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr33.Models
+{
+    public class Users
+    {
+        /// <summary> Код
+        public int Id { get; set; }
+        /// <summary> Фамилия
+        public string Lastname { get; set; }
+        /// <summary> Имя пользователя
+        public string Firstname { get; set; }
+
+        /// <summary> Отчество
+        public string Surname { get; set; }
+        /// <summary> Фотография
+        public byte[] Photo { get; set; }
+        /// <summary> Конструктор для заполнения объекта
+        public Users(string Lastname, string Firstname, string Surname, byte[] Photo)
+        {
+            this.Lastname = Lastname;
+            this.Firstname = Firstname;
+            this.Surname = Surname;
+            this.Photo = Photo;
+        }
+
+        /// <summary> Получить ФИО пользователя
+        public string ToFIO()
+        {
+            return $"{Lastname} {Firstname} {Surname}";
+        }
+    }
+}
