079d2dd98d232c79e994221a9f471deab82b7a5c
gobdanov <gobdanov@inbox.ru>
2026-03-20 12:44:47 +0500

    Разработанный пользовательский элемент Message. Белослудцева

---
+++ Items/Message.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Controls;
+using pr33.Classes.Common;
+using pr33.Models;
+
+namespace pr33.Items
+{
+    /// <summary>
+    /// Логика взаимодействия для Message.xaml
+    /// </summary>
+    public partial class Message : UserControl
+    {
+        public Message(Messages Messages, Users UserFrom)
+        {
+            InitializeComponent();
+            // Конвертируем изображение пользователя из массива байт в BitmapImage
+            imgUser.Source = BitmapFromArrayByte.LoadImage(UserFrom.Photo);
+            // Получаем ФИО
+            FIO.Content = UserFrom.ToFIO();
+            // Отображаем изображение
+            tbMessage.Text = Messages.Message;
+        }
+    }
+}
